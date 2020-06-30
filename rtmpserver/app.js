fe78d3afd546a69fcb446e40dafd7b93cddc5d19
Chirag Pipaliya <chiragpipaliya454@gmail.com>
2020-06-30 21:43:04 +0530

    initial commit

---
+++ rtmpserver/app.js
@@ -0,0 +1,27 @@
+const NodeMediaServer = require("node-media-server");
+
+const config = {
+  logType: 3,
+  rtmp: {
+    port: 1935,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 30,
+    ping_timeout: 60,
+  },
+  http: {
+    port: 8000,
+    allow_origin: "*",
+  },
+};
+
+let nms = new NodeMediaServer(config);
+
+nms.on("doneConnect", (id, args) => {
+  console.log(
+    "[NodeEvent on doneConnect]",
+    `id=${id} args=${JSON.stringify(args)}`
+  );
+});
+
+nms.run();
